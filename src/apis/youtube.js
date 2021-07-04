0ef1bd312c0bbab554329a117f243126c3f6b72a
kimo2121 <kareem.gaad@gmil.com>
2021-07-05 00:56:59 +0200

    few changes

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import KEY from "../apiKey";
+// import KEY from "../apiKey";
+const KEY = "AIzaSyAZFEie_d5QVqPZx0cP8hHioLNU9nj5XT8";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
